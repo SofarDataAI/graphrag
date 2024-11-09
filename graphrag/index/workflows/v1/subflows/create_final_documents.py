e7af948fa1c6d78b662b77292e5e2856b50407af
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-11-09 08:23:18 +0700

    Update based on v0.4.1

--- graphrag/index/workflows/v1/subflows/create_final_documents.py
+++ graphrag/index/workflows/v1/subflows/create_final_documents.py
@@ -8,37 +8,30 @@ from typing import cast
 import modin.pandas as pd
 from datashaper import (
     Table,
-    VerbCallbacks,
     VerbInput,
     verb,
 )
 from datashaper.table_store.types import VerbResult, create_verb_result
 
-from graphrag.index.cache import PipelineCache
 from graphrag.index.flows.create_final_documents import (
     create_final_documents as create_final_documents_flow,
 )
+from graphrag.index.utils.ds_util import get_required_input_table
 
 
 @verb(
     name="create_final_documents",
     treats_input_tables_as_immutable=True,
 )
-async def create_final_documents(
+def create_final_documents(
     input: VerbInput,
-    callbacks: VerbCallbacks,
-    cache: PipelineCache,
-    raw_content_text_embed: dict | None = None,
+    document_attribute_columns: list[str] | None = None,
     **_kwargs: dict,
 ) -> VerbResult:
     """All the steps to transform final documents."""
     source = cast(pd.DataFrame, input.get_input())
+    text_units = cast(pd.DataFrame, get_required_input_table(input, "text_units").table)
 
-    output = await create_final_documents_flow(
-        source,
-        callbacks,
-        cache,
-        raw_content_text_embed=raw_content_text_embed,
-    )
+    output = create_final_documents_flow(source, text_units, document_attribute_columns)
 
     return create_verb_result(cast(Table, output))
